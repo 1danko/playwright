7a7cc39daf410b566294969f427a867a28b0c5b9
Siarhei Malaichuk <guitarhoper@gmail.com>
2024-03-21 11:11:45 +0100

    finished guide 4

---
+++ tests/workshop_4/workshop_4.spec.ts
@@ -0,0 +1,40 @@
+import {test, expect} from '@playwright/test'
+
+test.skip('Handling Alerts', async ({page}) => {
+    await page.goto('file:///D:/TestAutomation/playwright_course_by_VasilShpak/playwright/tests/workshop_4/index.html');
+    let allertMessage = '';
+    page.on('dialog', async(dialog) => {
+        expect(dialog.type()).toBe('alert');
+        allertMessage = await dialog.message();
+        await dialog.accept();
+    });
+
+    await page.click('#show-alert');
+
+    expect(allertMessage).toBe('This is a simple alert.')
+})
+
+test.skip('Confirm Alert', async ({page}) => {
+    await page.goto('file:///D:/TestAutomation/playwright_course_by_VasilShpak/playwright/tests/workshop_4/index.html');
+    let allertMessage = '';
+    page.on('dialog', async(dialog) => {
+        allertMessage = await dialog.message();
+        await dialog.dismiss();
+    });
+
+    await page.click('#show-confirm');
+
+    expect(allertMessage).toBe('You clicked Cancel.')
+})
+
+test.only('Handling pop-ups', async ({page}) => {
+    await page.goto('file:///D:/TestAutomation/playwright_course_by_VasilShpak/playwright/tests/workshop_4/index.html');
+    const [popup] = await Promise.all([
+        page.waitForEvent('popup'),
+        page.click('#open-popup'),
+    ]);
+
+    await popup.waitForLoadState();
+
+    await popup.close();
+})
